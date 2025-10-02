a7140df16751cd7f57e07af5485eb83e82bd4c16
Alessandro Ferrari <aleferrarss@gmail.com>
2025-10-02 13:38:34 +0200

    Major update
    Major website update. Backuped old design

--- script/script.js
+++ script/script.js
@@ -49,4 +49,24 @@
         document.getElementById('loading-mask').style.display = 'none';
     }
 
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() {addSticky()};
+
+    // Get the navbar
+    var sidebar = document.getElementById("sidebar");
+    console.log("F1 OK");
+
+    // Get the offset position of the navbar
+    var sticky = sidebar.offsetTop;
+    console.log("F2 OK");
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function addSticky() {
+        if (window.scrollY >= sticky) {
+            sidebar.classList.add("sticky")
+        } else {
+            sidebar.classList.remove("sticky");
+        }
+    }
+
 
